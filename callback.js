64cef9ff2655746dd106cd46a2e86b03490417a4
Partha sarathi <142418080+sarathi-2003@users.noreply.github.com>
2023-09-22 19:59:24 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,58 @@
+
+        
+        let timer=10;
+        let time = document.getElementById('onscreen');
+        setTimeout(() => {
+            time.innerHTML = timer--;
+         setTimeout(() =>{
+         time.className = "10 to 1 counting";
+                time.innerHTML = timer--;
+        setTimeout(() =>{
+          time.className = "10 to 1 counting";
+              time.innerHTML = timer--;
+        setTimeout(() =>{
+          time.className = "10 to 1 counting";
+               time.innerHTML = timer--;
+                 setTimeout(() =>{
+            time.className = "10 to 1 counting";
+                     time.innerHTML = timer--;
+            setTimeout(() =>{
+              time.className = "10 to 1 counting";
+                  time.innerHTML = timer--;
+                 setTimeout(() =>{
+                   time.className = "10 to 1 counting";
+                            time.innerHTML = timer--;
+            setTimeout(() =>{
+                        time.className = "10 to 1 counting";
+                            time.innerHTML = timer--;
+             setTimeout(() =>{
+                            time.className = "10 to 1 counting";
+                                            time.innerHTML = timer--;
+                 setTimeout(() =>{
+                                time.className = "10 to 1 counting";
+                             time.innerHTML = timer--;
+                  setTimeout(() =>{
+                  
+                              time.style.fontSize = "40px";
+                                      time.style.maxWidth = "310px";
+                                  time.className = "10 to 1 counting";
+                                  
+                   time.innerHTML = "Happy Independence Day!!!"  
+                                                
+              },1000)
+
+                       },1000)
+
+           },1000)
+                    },1000)
+               },1000)
+
+           },1000)
+                 },1000)
+            
+        },1000)     
+    
+    },1000)
+              },1000)
+               }, 1000); 
+    
